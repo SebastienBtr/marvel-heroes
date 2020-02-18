43eab092320c2dabaaee0d89a74a9472a70745f8
SebastienBtr <sebastien29520@gmail.com>
2020-02-18 09:59:21 +0100

    data imports

--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -2,8 +2,83 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({ node: 'http://localhost:9200' })
-const heroesIndexName = 'heroes'
+const client = new Client({ node: 'http://localhost:9200' })
+const indexName = 'heroes'
 
-// TODO
-console.log("TODO ;-)");
+async function run() {
+  // Create index
+  await checkIndices();
+  // Import data
+  importData();
+}
+
+// Read the csv and import the data to elasticsearch
+function importData() {
+  let heroes = [];
+
+  // Read CSV file
+  fs.createReadStream('all-heroes.csv')
+    .pipe(csv({
+      separator: ','
+    }))
+    .on('data', (data) => {
+      heroes.push({
+        'id': data.id,
+        'name': data.name,
+        'description': data.description,
+        'imageUrl': data.imageUrl,
+        'backgroundUrl': data.backgroundUrl,
+        'secretIdentities': data.secretIdentities,
+        'aliases': data.aliases,
+        'partners': data.partners,
+      });
+    })
+    .on('end', () => {
+      console.log('push data');
+      sendData(heroes).then(() => {
+        console.log('End');
+        client.close();
+      }, (err) => {
+        console.log('End because of an error');
+        console.trace(err);
+        client.close();
+      });
+    });
+}
+
+// Send the data to elasticsearch
+async function sendData(dataSet) {
+  return new Promise(async function (resolve, reject) {
+    const { body: bulkResponse } = await client.bulk(createBulkInsertQuery(dataSet));
+    if (bulkResponse.errors) {
+      reject(errors);
+    } else {
+      console.log(`${dataSet.length} data sent`);
+    }
+    resolve();
+  });
+}
+
+// Format data for a bulk insert
+function createBulkInsertQuery(dataSet) {
+  const body = dataSet.reduce((acc, data) => {
+    acc.push({ index: { _index: indexName, _type: '_doc', _id: data.id } })
+    delete data.id;
+    acc.push(data);
+    return acc
+  }, []);
+
+  return { body };
+}
+
+// Create an index if not exist
+async function checkIndices() {
+  const { body: exist } = await client.indices.exists({ index: indexName });
+  if (exist) {
+    console.log('index already exists');
+  } else {
+    await client.indices.create({ index: indexName });
+  }
+}
+
+run().catch(console.error);
--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -7,5 +7,51 @@ var mongoUrl = "mongodb://localhost:27017";
 const dbName = "marvel";
 const collectionName = "heroes";
 
-// TODO
-console.log("TODO ;-)");
+async function run() {
+  // Import data
+  importData();
+}
+
+// Read the csv and import the data to mongo db
+function importData(client) {
+  let heroes = [];
+
+  // Read CSV file
+  fs.createReadStream('all-heroes.csv')
+    .pipe(csv({
+      separator: ','
+    }))
+    .on('data', (data) => {
+      heroes.push(data);
+    })
+    .on('end', () => {
+      console.log('push data');
+      sendData(heroes).then(() => {
+        console.log('End');
+      }, (err) => {
+        console.log('End because of an error');
+        console.trace(err);
+      });
+    });
+}
+
+// Send the data to mongo db
+async function sendData(dataSet) {
+  return new Promise(async function (resolve, reject) {
+    const client = await MongoClient.connect(mongoUrl).catch(err => {
+      reject(err);
+    });
+
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    await collection.insertMany(dataSet).catch(err => {
+      client.close();
+      reject(err);
+    });
+
+    client.close();
+    resolve();
+  });
+}
+
+run().catch(console.error);
